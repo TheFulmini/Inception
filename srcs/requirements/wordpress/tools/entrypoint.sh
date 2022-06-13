22691380acab4266d114f42cf2f12497498ccc68
Alexandro Fulmini <58033778+TheFulmini@users.noreply.github.com>
2022-06-13 18:12:18 +0200

    Update entrypoint.sh

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -f "wp-config.php" ]; then
 
     wp config create --dbname=$DB_NAME --dbhost=$DB_HOST --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
 
-    wp core install --url=$WP_URL --title="${WP_TITLE}" --admin_name=$WP_ADMIN_NAME \
+    wp core install --url=$WP_URL --title="${WP_TITLE}" --admin_name=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL --allow-root
 
     wp user create $WP_USR_NAME $WP_USR_MAIL --user_pass=$WP_USR_PWD --role=$WP_USR_ROLE --allow-root
 
